f878a711200148f84585dec0c262a5ea4ac37569
Zhaoyang <yangzhao635226@163.com>
2020-03-25 13:24:21 +0800

    项目配置更改

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,8 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Admin Template',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: 'Diboot Element Admin',
+      logo: 'https://www.diboot.com/logo.png'
     }
   }
 }
--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: 'Diboot Element Admin',
 
   /**
    * @type {boolean} true | false
