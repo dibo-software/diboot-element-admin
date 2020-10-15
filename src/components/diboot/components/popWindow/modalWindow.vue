47cb7719309260965167d19af0fc3ce15cad660b
yangzhao635226@163.com <zhaozhaoyang123456>
2020-10-15 16:22:35 +0800

    add: 添加通用弹窗组件

---
+++ src/components/diboot/components/popWindow/modalWindow.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-dialog
+    :title="title"
+    :width="width"
+    :visible.sync="state.visible"
+  >
+    <slot name="content" />
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="close">取 消</el-button>
+      <el-button type="primary" @click="onConfirm">确 定</el-button>
+    </span>
+  </el-dialog>
+</template>
+<script>
+export default {
+  name: 'ModalWindow',
+  props: {
+    title: {
+      type: String,
+      default: '窗口'
+    },
+    width: {
+      type: Number,
+      default: 1200
+    }
+  },
+  data() {
+    return {
+      state: {
+        visible: false,
+        bodyHeight: 320
+      }
+    }
+  },
+  created() {
+    this.state.bodyHeight = window.innerHeight - 110
+  },
+  methods: {
+    open() {
+      this.state.visible = true
+    },
+    onConfirm() {
+      this.confirm()
+      this.state.visible = false
+    },
+    confirm() {
+
+    },
+    close() {
+      this.state.visible = false
+    }
+  }
+}
+</script>
