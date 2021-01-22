8e410619237800d24d16afcdb9f28d89ed6c3802
yangzhao635226@163.com <zhaozhaoyang123456>
2021-01-22 16:39:18 +0800

    + 用户导入导出功能项

---
+++ src/views/orgStructure/orgUser/userImport.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-dialog
+    class="excel-import"
+    :visible.sync="visible"
+    title="数据上传"
+    @closed="close"
+  >
+    <excel-import
+      v-if="visible"
+      :example-url="`${baseApi}/downloadExample`"
+      :upload-url="`${baseApi}/upload`"
+      :preview-url="`${baseApi}/preview`"
+      :preview-save-url="`${baseApi}/previewSave`"
+      :fields-required="fieldsRequired"
+      @finishedUpload="handleFinishedUpload"
+    />
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="close">关闭</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import ExcelImport from '@/components/diboot/components/import/ExcelImport'
+
+export default {
+  name: 'IamUserImport',
+  components: {
+    ExcelImport
+  },
+  props: {
+    currentNodeId: {
+      type: String,
+      default: '0'
+    }
+  },
+  data() {
+    return {
+      baseApi: '/iam/user/excel',
+      visible: false
+    }
+  },
+  computed: {
+    /**
+     * 后端需要的固定参数
+     * @returns {{}}
+     */
+    fieldsRequired: function() {
+      return { orgId: this.currentNodeId }
+    }
+  },
+  methods: {
+    open() {
+      this.visible = true
+    },
+    /**
+     * 刷新数据
+     */
+    handleFinishedUpload() {
+      this.$emit('complete')
+      this.visible = false
+    },
+    close() {
+      this.visible = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/orgStructure/orgUser/userList.vue
+++ src/views/orgStructure/orgUser/userList.vue
@@ -1,37 +1,71 @@
 <template>
-  <div class="orgUserList">
-    <el-row>
-      <el-col :lg="20" :md="24">
-        <div class="filter-container">
-          <el-input v-model="queryParam.realname" placeholder="姓名" style="width: 200px;" class="filter-item" @keyup.enter.native="onSearch" />
-          <el-select v-model="queryParam.status" placeholder="状态" style="width: 200px;" class="filter-item" @keyup.enter.native="onSearch">
-            <el-option
-              v-for="(item, index) in more.userStatusKvList"
-              :key="index"
-              :value="item.v"
-              :label="item.k"
-            />
-          </el-select>
-          <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="onSearch">
-            查询
-          </el-button>
-          <el-button class="filter-item" type="info" icon="el-icon-refresh" @click="reset">
-            重置
-          </el-button>
-        </div>
-      </el-col>
-      <el-col :lg="4" :md="24" style="text-align: right;">
-        <el-button v-permission="['create']" class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-plus" @click="$refs.form.open(undefined)">
-          新建
-        </el-button>
-        <el-button v-permission="['import']" class="filter-item" style="margin-left: 10px;" type="default" icon="el-icon-plus" @click="$refs.form.open(undefined)">
-          批量导入
-        </el-button>
-        <el-button v-permission="['export']" class="filter-item" style="margin-left: 10px;" type="default" icon="el-icon-plus" @click="$refs.form.open(undefined)">
-          导出
-        </el-button>
-      </el-col>
-    </el-row>
+  <div class="app-container">
+
+    <div class="table-page-search-wrapper">
+      <el-form :inline="true" label-width="100px">
+        <el-row :gutter="18">
+          <el-col :md="8" :sm="24">
+            <el-form-item label="姓名" style="width: 100%;">
+              <el-input v-model="queryParam.realname" placeholder="姓名" style="width: 200px;" class="filter-item" @keyup.enter.native="onSearch" />
+            </el-form-item>
+          </el-col>
+          <el-col :md="8" :sm="24">
+            <el-form-item label="工号" style="width: 100%;">
+              <el-input v-model="queryParam.userNum" placeholder="工号" style="width: 200px;" class="filter-item" @keyup.enter.native="onSearch" />
+            </el-form-item>
+          </el-col>
+          <template v-if="advanced">
+            <el-col :md="8" :sm="24">
+              <el-form-item label="性别" style="width: 100%;">
+                <el-select v-model="queryParam.gender" placeholder="请选择性别" style="width: 100%;">
+                  <el-option
+                    v-for="kv in more.genderKvList"
+                    :key="kv.v"
+                    :value="kv.v"
+                    :label="kv.k"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :md="8" :sm="24">
+              <el-form-item label="电话" style="width: 100%;">
+                <el-input v-model="queryParam.mobilePhone" placeholder="电话" style="width: 200px;" class="filter-item" @keyup.enter.native="onSearch" />
+              </el-form-item>
+            </el-col>
+            <el-col :md="8" :sm="24">
+              <el-form-item label="邮箱" style="width: 100%;">
+                <el-input v-model="queryParam.email" placeholder="邮箱" style="width: 200px;" class="filter-item" @keyup.enter.native="onSearch" />
+              </el-form-item>
+            </el-col>
+          </template>
+          <el-col :md="!advanced && 8 || 24" :sm="24">
+            <span class="table-page-search-submitButtons" :style="advanced && { float: 'right', overflow: 'hidden' } || {} ">
+              <el-button v-waves type="primary" icon="el-icon-search" @click="onSearch">
+                查询
+              </el-button>
+              <el-button style="margin-left: 8px" type="info" icon="el-icon-refresh" @click="reset">
+                重置
+              </el-button>
+              <el-link type="primary" :underline="false" style="margin-left: 8px" @click="toggleAdvanced">
+                {{ advanced ? '收起' : '展开' }}
+                <i :class="advanced ? 'el-icon-arrow-up' : 'el-icon-arrow-down'" />
+              </el-link>
+            </span>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+    <div class="table-operator">
+      <el-button v-permission="['create']" class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-plus" @click="$refs.form.open(undefined)">
+        新建
+      </el-button>
+      <el-button v-permission="['import']" class="filter-item" style="margin-left: 10px;" type="default" icon="el-icon-upload2" @click="$refs.userImport.open()">
+        批量导入
+      </el-button>
+      <el-button v-permission="['export']" class="filter-item" style="margin-left: 10px;" type="default" :icon="exportLoadingData ? 'el-icon-loading' : 'el-icon-download'" @click="exportData">
+        导出
+      </el-button>
+    </div>
     <el-table
       v-loading="loadingData"
       :data="list"
@@ -148,6 +182,7 @@
       :current-node-id="currentNodeId"
       @complete="getList"
     />
+    <user-import ref="userImport" :current-node-id="currentNodeId" @complete="getList" />
   </div>
 </template>
 <script>
@@ -155,6 +190,7 @@ import userDetail from './userDetail'
 import userForm from './userForm'
 import waves from '@/directive/waves'
 import list from '@/components/diboot/mixins/list'
+import userImport from '@/views/orgStructure/orgUser/userImport'
 import userPositionRefForm from '@/views/orgStructure/orgUser/userPositionRefForm'
 
 export default {
@@ -162,6 +198,7 @@ export default {
   components: {
     userDetail,
     userForm,
+    userImport,
     userPositionRefForm
   },
   directives: { waves },
