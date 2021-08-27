af847e64ce4c171c8dcd2529c9e1193ec713074a
yangzhao635226@163.com <zhaozhaoyang123456>
2021-08-27 15:39:26 +0800

    + 支持开区间的日期区间查询

---
+++ src/components/diboot/components/dateRange/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <el-date-picker
+      v-model="value[0]"
+      style="width: calc(50% - 15px);"
+      type="date"
+      clearable
+      :format="format"
+      :value-format="format"
+      placeholder="开始日期"
+      @change="onChange"
+    />
+    ~
+    <el-date-picker
+      v-model="value[1]"
+      style="width: calc(50% - 15px);"
+      type="date"
+      clearable
+      :format="format"
+      :value-format="format"
+      placeholder="结束日期"
+      @change="onChange"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DateRange',
+  props: {
+    value: {
+      type: Array,
+      default: () => [undefined, undefined]
+    },
+    format: {
+      type: String,
+      default: 'yyyy-MM-dd'
+    },
+    allowClear: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    onChange() {
+      const { value } = this
+      this.$emit('input', value)
+      this.$emit('change', value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
